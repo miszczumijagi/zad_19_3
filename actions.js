ed27a0d85a29cb3f3ac0b5cc039b45a8af36e41f
Kamil Stangret <kamilstangret@wp.pl>
2018-03-08 18:41:42 +0100

    Add actions.js file with content

---
+++ actions.js
@@ -0,0 +1,45 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const DELETE_COMMENT = 'DELETE_COMMENT';
+const THUMB_UP = 'THUMB_UP_COMMENT';
+const THUMB_DOWN = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id,text) {
+	return {
+		type: EDIT_COMMENT,
+		id,
+		text
+	}
+}
+
+function deleteComment(id) {
+	return {
+		type: DELETE_COMMENT,
+		id
+	}
+}
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id
+	}
+}
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id
+	}
+}
+
